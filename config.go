e4b03340592c78fc6ce2c87cb108a17cfcedc96d
stevenzack <stevenzack@qq.com>
2021-01-18 15:35:08 +0800

    remove-maxBytes-limit

--- config.go
+++ config.go
@@ -28,9 +28,9 @@ type (
 		service.ServiceConf
 		Host     string `json:",default=0.0.0.0"`
 		Port     int
-		Verbose  bool  `json:",optional"`
-		MaxConns int   `json:",default=10000"`
-		MaxBytes int64 `json:",default=1048576,range=[0:8388608]"`
+		Verbose  bool `json:",optional"`
+		MaxConns int  `json:",default=10000"`
+		MaxBytes int64
 		// milliseconds
 		Timeout      int64         `json:",optional"`
 		CpuThreshold int64         `json:",default=900,range=[0:1000]"`
